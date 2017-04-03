7d2ad88b96374dcb7572e4ffbb31a2f0c687dbc1
Clara Anton Fernandez <caf@nibio.no>
2017-04-03 14:26:28 +0200

    Initial commit

---
+++ R/BAL.R
@@ -0,0 +1,13 @@
+ 
+
+PBAL <-  function(BA){
+  ord.x <- order(BA)
+  BAo <- sum(BA[ord.x]) - cumsum(BA[ord.x])
+  ## ties
+  is.tie <- rev(duplicated(BA[order(BA, decreasing = TRUE)]))
+  BAo[is.tie] <- BAo[is.tie] - BA[ord.x][is.tie]
+  ## return it to the original form
+  BAo <- BAo[match(1:length(BAo), ord.x)]
+  return(BAo)
+}
+
